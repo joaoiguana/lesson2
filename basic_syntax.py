12e775ce616a05ba636e6104fb37749a1ea2db1a
Joao Iguana <jiboiadamor@gmail.com>
2024-03-12 11:13:12 +0000

    practiced python variables and data types

--- basic_syntax.py
+++ basic_syntax.py
@@ -5,3 +5,13 @@ print('Hello, World!') # This is a inline comment
 message = "Hello, Python!"
 number = 42
 pi = 3.1415
+
+name = "João Garcia" # String
+age = 31 # Integer
+height = 1.85 # Floating Point
+is_adult = True # Boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is adult:", is_adult)
